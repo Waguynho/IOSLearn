4d55b0ad1ed61d77691ccc33defad48849d49cfb
Wagner dos Santos <wagner.santos@sigga.com>
2022-01-06 14:21:27 -0300

    Ternary

--- PlayGroundOne.playground/Contents.swift
+++ PlayGroundOne.playground/Contents.swift
@@ -1,6 +1,11 @@
 import UIKit
 
 var greeting:String = "Hello Ws!"
-var age:Int = 63
+var age:Int = 55
 let isGreeting = true
 print("Say:  \(greeting) \nWith number: \(age)")
+var message = age > 60 ? "Idoso" : "Jovem"
+print(message)
+let result:Double = (Double("13.5")! + 1.6)
+
+

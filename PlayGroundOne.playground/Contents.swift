41443be2d0d3f1338bca3512d398e8326cad6a5a
Wagner dos Santos <wagner.santos@sigga.com>
2022-01-06 14:42:42 -0300

    Loop in Dictonary

--- PlayGroundOne.playground/Contents.swift
+++ PlayGroundOne.playground/Contents.swift
@@ -8,4 +8,12 @@ var message = age > 60 ? "Idoso" : "Jovem"
 print(message)
 let result:Double = (Double("13.5")! + 1.6)
 
+var dicionary: [Int:String] = [
+    1:"Maria",
+    2:"Joao",
+    3:"Helena"
+]
 
+for (cod, name) in dicionary.reversed() {
+    print("Code: \(cod) name: \(name)")
+}

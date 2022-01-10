aa1188ef86398ca12d26a9cd90a86124f1294f3d
Wagner dos Santos <wagner.santos@sigga.com>
2022-01-10 13:43:34 -0300

    Completed tasks view

--- CustomAlert/CustomAlert/Views/CompletedTasksView.swift
+++ CustomAlert/CustomAlert/Views/CompletedTasksView.swift
@@ -3,10 +3,28 @@ import SwiftUI
 
 struct CompletedTasksView: View {
     
-    
+    // MARK: - Properties
+    @ObservedObject var dataStore = DataStore.shared
+    @Environment(\.presentationMode) var presentationMode
     
     // MARK: - View
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            List {
+                ForEach(dataStore.completedTodoItems) { todoItem in
+                    ListCellView(todoItem: todoItem)
+                }
+            }
+            .listStyle(InsetGroupedListStyle())
+            .navigationTitle("Completed Tasks")
+            .navigationBarItems(trailing: Button(action: {
+                presentationMode.wrappedValue.dismiss()
+            }) {
+                Text("Done")
+            })
+            .textFieldAlert(isPresented: $dataStore.alertShowing) {
+                TextFieldAlert(action: dataStore.currentAction!)
+            }
+        }
     }
 }

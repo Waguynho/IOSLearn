e6f8fa694511f4536808cdcdb9da4f59e2729603
Wagner dos Santos <wagner.santos@sigga.com>
2022-01-18 12:35:22 -0300

    Create Ws Workspace

---
+++ MqttChat/Chat/Chat.h
@@ -0,0 +1,18 @@
+//
+//  Chat.h
+//  Chat
+//
+//  Created by Wagner Santos on 18/01/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Chat.
+FOUNDATION_EXPORT double ChatVersionNumber;
+
+//! Project version string for Chat.
+FOUNDATION_EXPORT const unsigned char ChatVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Chat/PublicHeader.h>
+
+
--- MqttChat/MqttChat/Views/Chat.swift
+++ MqttChat/MqttChat/Views/Chat.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Chat
 
 struct Chat: View {
     @ObservedObject var model = ChatModel()

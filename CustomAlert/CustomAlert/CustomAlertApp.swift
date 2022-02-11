7d49f244e09ec2eb934a3d8f5cf9e975c8a40948
Wagner dos Santos <wagner.santos@sigga.com>
2022-02-11 10:06:59 -0300

    Custom Alert -  NewRelic - with script in xcode on build phases

--- CustomAlert/CustomAlert/CustomAlertApp.swift
+++ CustomAlert/CustomAlert/CustomAlertApp.swift
@@ -4,9 +4,19 @@ import SwiftUI
 @main
 struct CustomAlertApp: App {
     
+    private let wsToken = "AA172c324867c49c09ddba21fb8690f87b4ae0acb6-NRMA"
+    private let temporaryAppToken = "AAd59a623a06e61d7569e1b9902b6d767ec946af4d-NRMA"
+    private let wsToken1 = "AA527f8eacca956cb6e5452f00aaa0f98af2b42bd6-NRMA"
+    
     init(){
-        NewRelic.enableFeatures(NRMAFeatureFlags.NRFeatureFlag_NetworkRequestEvents)
-        NewRelic.start(withApplicationToken: "AA172c324867c49c09ddba21fb8690f87b4ae0acb6-NRMA")
+//        NewRelic.enableFeatures(NRMAFeatureFlags.NRFeatureFlag_NetworkRequestEvents)
+//        NewRelic.enableFeatures(NRMAFeatureFlags.NRFeatureFlag_RequestErrorEvents)
+//        NewRelic.enableFeatures(NRMAFeatureFlags.NRFeatureFlag_CrashReporting)
+//        NewRelic.enableFeatures(NRMAFeatureFlags.NRFeatureFlag_HandledExceptionEvents)
+//        NewRelic.enableFeatures(NRMAFeatureFlags.NRFeatureFlag_InteractionTracing)
+//        NewRelic.enableFeatures(NRMAFeatureFlags.NRFeatureFlag_GestureInstrumentation)
+        
+        NewRelic.start(withApplicationToken:  temporaryAppToken)
     }
     
     var body: some Scene {
